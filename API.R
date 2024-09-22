9464c6145c17b563440420e0e2162d4f58d1f76a
CiocGitAcc <ciocandumitru@gmail.com>
2024-09-22 20:35:29 +0200

    Update API.R

--- API.R
+++ API.R
@@ -1,25 +1,14 @@
-###############################################################################
-# PROJECT: API
-# SCRIPT : Test version to run a hello world api call
-###############################################################################
-# Libraries
-library(plumber)  
-
-# api.R
 library(plumber)
 
 #* @get /fridge
-# Returns the contents of the fridge
 function() {
   list(milk = 2, eggs = 12, cheese = 1)
 }
 
 #* @get /shopping-list
-# Returns a basic shopping list
 function() {
   list(bread = 2, butter = 1, eggs = 6)
 }
 
-# Plumb and run the API
 pr <- plumber::plumb("/usr/local/src/API.R")
 pr$run(host = "0.0.0.0", port = 8080)
